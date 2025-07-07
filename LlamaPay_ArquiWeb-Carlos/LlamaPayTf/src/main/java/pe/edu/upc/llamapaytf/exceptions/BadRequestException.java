0ead319bd286c6d81c4b5d2f7e2653afe62cb81c
Ivana Panduro Rodriguez <u20201c143@upc.edu.pe>
2025-07-07 13:26:37 -0500

    solo front

--- LlamaPay_ArquiWeb-Carlos/LlamaPayTf/src/main/java/pe/edu/upc/llamapaytf/exceptions/BadRequestException.java
+++
@@ -1,8 +0,0 @@
-package pe.edu.upc.llamapaytf.exceptions;
-
-public class BadRequestException extends RuntimeException {
-    public BadRequestException(String message) {
-        super(message);
-    }
-}
-
--- LlamaPay_ArquiWeb-Carlos/LlamaPayTf/src/main/java/pe/edu/upc/llamapaytf/exceptions/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package pe.edu.upc.llamapaytf.exceptions;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String mensaje) {
-        super(mensaje);
-    }
-}
